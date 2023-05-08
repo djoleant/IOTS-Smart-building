3b0250e94c073530d0cc8fdb9a75dd9654a752bb
Djordje Antic <djoleant@iCloud.com>
2023-05-08 22:41:43 +0200

    [gRPC] Room proto and Room Service add

--- SmartBuilding_gRPC/SmartBuilding_gRPC/Program.cs
+++ SmartBuilding_gRPC/SmartBuilding_gRPC/Program.cs
@@ -46,6 +46,7 @@ builder.Services.AddEndpointsApiExplorer();
 var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
+app.MapGrpcService<RoomService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
---
+++ SmartBuilding_gRPC/SmartBuilding_gRPC/Services/RoomService.cs
@@ -0,0 +1,30 @@
+﻿using Grpc.Core;
+using SmartBuilding_gRPC;
+using SmartBuilding_gRPC.Models;
+using SmartBuilding_gRPC.Protos;
+
+namespace SmartBuilding_gRPC.Services
+{
+    public class RoomService : Room.RoomBase
+    {
+        private readonly ILogger<RoomService> _logger;
+        private readonly SmartBuildingDbContext _dbContext;
+
+        public RoomService(ILogger<RoomService> logger, SmartBuildingDbContext dbContext)
+        {
+            _logger = logger;
+            _dbContext = dbContext;
+        }
+
+        public override Task<RoomVals> GetRoominfo(RoomValsId request, ServerCallContext context)
+        {
+            RoomVals output = new RoomVals();
+
+
+
+            return Task.FromResult(output);
+        }
+    }
+}
+
+// C:\Users\Djole\Desktop\git\IoTS-Smart-building\SmartBuilding_gRPC\SmartBuilding_gRPC\obj\Debug\net6.0\Protos\GreetGrpc.cs
