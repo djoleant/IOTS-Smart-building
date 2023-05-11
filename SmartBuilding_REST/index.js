5152952715519bcdcf3fbda5c99c20a03c174489
Emilija Cojbasic <ecojbasic@elfak.rs>
2023-05-12 00:06:55 +0200

    REST implementation

--- SmartBuilding_REST/index.js
+++ SmartBuilding_REST/index.js
@@ -1,30 +1,26 @@
 const express = require('express');
-const sql = require('mssql');
+const db = require('./db-info.js');
 
-(async () => {
-    await sql.connect({
-        server: '(localhost)\\SmartBuildingDB',
-        database: 'SmartBuildingDB'
-    });
+
     const app = express();
     app.use(express.json());
     const port = 3000;
     
     // app.get('/Rooms', async (req, res) => {
-    //   const results = await sql.query('SELECT * FROM Rooms');
+    //   const results = await db.promise().query('SELECT * FROM Rooms');
     //   res.status(200).send(results[0]);
     // })
     
-    app.get('/Room/:id', async (req, res) => {
-        const results = await sql.query(`SELECT * FROM Rooms WHERE ID = '${req.params.valueId}'`);
+    app.get('/Room/:valueId', async (req, res) => {
+        const results = await db.promise().query(`SELECT * FROM Rooms WHERE ValueId = '${req.params.valueId}'`);
         res.status(200).send(results[0]);
     })
     
     app.post('/Room', async (req, res) => {
         try {
             const { valueId, timestamp, light, humidity, temperature, co2 } = req.body;
-            await sql.query(`INSERT INTO Rooms(ValueId,Timestamp,Light,Humidity,Temperature,CO2) VALUES('${valueId}', '${timestamp}', '${light}', '${humidity}', '${temperature}', '${co2}')`);
-            const results = await sql.query(`SELECT * FROM SENSOR_VALUES WHERE ID = '${valueId}'`);
+            await db.promise().query(`INSERT INTO Rooms(ValueId,Timestamp,Light,Humidity,Temperature,CO2) VALUES('${valueId}', '${timestamp}', '${light}', '${humidity}', '${temperature}', '${co2}')`);
+            const results = await db.promise().query(`SELECT * FROM SENSOR_VALUES WHERE ValueId = '${valueId}'`);
             res.status(201).send(results[0]);
         }
         catch (err) {
@@ -35,8 +31,8 @@ const sql = require('mssql');
     app.put('/Room', async (req, res) => {
         try {
             const { valueId, timestamp, light, humidity, temperature, co2 } = req.body;
-            await sql.query(`UPDATE Rooms SET ValueId='${valueId}',Timestamp='${timestamp}',Light='${light}',Humidity='${humidity}',Temperature='${temperature}',CO2='${co2}' WHERE ValueId = '${valueId}'`);
-            const results = await sql.query(`SELECT * FROM Rooms WHERE ID = '${valueId}'`);
+            await db.promise().query(`UPDATE Rooms SET ValueId='${valueId}',Timestamp='${timestamp}',Light='${light}',Humidity='${humidity}',Temperature='${temperature}',CO2='${co2}' WHERE ValueId = '${valueId}'`);
+            const results = await db.promise().query(`SELECT * FROM Rooms WHERE ValueId = '${valueId}'`);
             res.status(201).send(results[0]);
         }
         catch (err) {
@@ -45,8 +41,8 @@ const sql = require('mssql');
     })
     
     
-    app.delete('/Room/:id', async (req, res) => {
-        await sql.query(`DELETE FROM Rooms WHERE ValueId = '${req.params.valueId}'`);
+    app.delete('/Room/:valueId', async (req, res) => {
+        await db.promise().query(`DELETE FROM Rooms WHERE ValueId = '${req.params.valueId}'`);
         res.status(200).send({ msg: 'Successfully deleted' });
     })
     
@@ -54,5 +50,4 @@ const sql = require('mssql');
         console.log(`Example app listening on port ${port}`)
     })
 
-})()    
 
